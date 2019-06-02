a7d295277de09f1f2535e78bd9d28ce528d79648
kevinhawryluk <kevin.hawryluk@gmail.com>
2019-06-02 18:52:43 -0500

    Small Formatting change

--- app/src/main/java/edu/uchicago/kjhawryluk/profinal2019/MainActivity.java
+++ app/src/main/java/edu/uchicago/kjhawryluk/profinal2019/MainActivity.java
@@ -97,6 +97,7 @@ public class MainActivity extends AppCompatActivity implements NasaImageListAdap
         // Get the SearchView and set the searchable configuration
         SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
         SearchView searchView = (SearchView) menu.findItem(R.id.searchBar).getActionView();
+
         // Assumes current activity is the searchable activity
         searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
         searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
